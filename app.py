4f9ca6fb7a7c15e96e4fa88389597d1f34821faf
Seabstian Hendel <sebastian@hendel-running.de>
2025-02-21 16:06:22 +0100

    init git repo

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Tutorial App", page_icon="📚", layout="wide")
+
+st.title("Willkommen zur Tutorial App 🎥")
+
+st.write("Wähle eine Seite aus der Seitenleiste, um ein Tutorial anzusehen.")
---
+++ pages/tutorial_1.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Tutorial 1: Einführung in Python")
+
+st.video("videos/tutorial_80mb.mp4")  # BeispielVideo
+
+st.write("Hier lernst du die Grundlagen vom Portal.")
